1b43ee527a6d06f920503da3c4bb391c857c8b6e
Matty <matty258987@gmail.com>
2022-01-29 20:36:34 +0000

    more curves

--- test.r
+++ test.r
@@ -18,6 +18,14 @@ plot(iris$Petal.Length, iris$Petal.Width,
 
 
 plot(cos, 0, 2*pi)
+plot(exp, 1, 5)
+plot(dnorm, -3, +3, 
+     col = "#cc0000", 
+     lwd = 5, 
+     main = "Standard Normal Distribution", 
+     xlab = "z-scores", 
+     ylab = "Density")
+
 
 
 
