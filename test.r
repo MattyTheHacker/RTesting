ab6ae598d9ce37a30860a550cd48bbfffa91094a
Matty <matty258987@gmail.com>
2022-01-29 20:35:41 +0000

    add curve plotting

--- test.r
+++ test.r
@@ -5,6 +5,19 @@ plot(iris$Species)
 plot(iris$Petal.Length)
 plot(iris$Species, iris$Petal.Width)
 plot(iris$Petal.Length, iris$Petal.Width)
+plot(iris)
+
+
+plot(iris$Petal.Length, iris$Petal.Width,
+     col = "#cc0000",
+     pch = 19,
+     main = "Iris: Petal Length v Petal Width",
+     xlab = "Petal Length",
+     ylab = "Petal Width")
+
+
+
+plot(cos, 0, 2*pi)
 
 
 
